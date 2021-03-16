79441bf8d4d4fff5703068334000d1ffca6e59c1
Rebecca <t820134@spark.co.nz>
2021-03-17 12:33:53 +1300

    handle key down event

--- modalWindow/index.js
+++ modalWindow/index.js
@@ -4,17 +4,24 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 function handleOpenClick() {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
 }
 
 function handleCloseClick() {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+}
+
+function handleKeyDown(e) {
+	e.key === "Escape" && !modal.classList.contains("hidden")
+		? handleCloseClick()
+		: null;
 }
 
 btnCloseModal.addEventListener("click", handleCloseClick);
 overlay.addEventListener("click", handleCloseClick);
+document.addEventListener("keydown", handleKeyDown);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
 	btnsOpenModal[i].addEventListener("click", handleOpenClick);
